f0dbbed778c50f511a1bac479d63429c3a9b75d6
Mohamad Ektelat <71977816+mohamadektelat@users.noreply.github.com>
2022-07-28 14:37:38 +0300

    Documentation

--- OfflinePart.py
+++ OfflinePart.py
@@ -1,20 +1,16 @@
 # -------------------------------------------------Imports--------------------------------------------------------------
-
 import os
-from dataclasses import dataclass
-import time
 import re
-from functools import reduce
-
 from classes import *
 
 
-# -------------------------------------------------Globals--------------------------------------------------------------
-
-
 # --------------------------------------------------Function------------------------------------------------------------
-
 def print_line_from_dict(key: str, dict: {}) -> None:
+    """
+    This function prints line from dict in a beautifully format.
+    :param key: given word.
+    :param dict: The given dictionary.
+    """
     print("=" * 20)
     print("key: ", key)
     print("-" * 20)
@@ -23,6 +19,13 @@ def print_line_from_dict(key: str, dict: {}) -> None:
 
 
 def mapping_files_in_folders(dir_name: str, traversed: list = [], results: list = []) -> list:
+    """
+    This function responsible for mapping the given files.
+    :param dir_name: The path for the given directory.
+    :param traversed: List of all the paths of the directories.
+    :param results: reference for the results list.
+    :return: List of the results.
+    """
     dirs = os.listdir(dir_name)
     if dirs:
         for f in dirs:
@@ -36,6 +39,13 @@ def mapping_files_in_folders(dir_name: str, traversed: list = [], results: list 
 
 
 def add_new_word_to_data_base(line_in_file, path, dict: {}, trie):
+    """
+    This function adds the lines from the files to our dictionary.
+    :param line_in_file: given line from the file.
+    :param path: The location of the file that contains the specific line.
+    :param dict: The given dictionary.
+    :param trie: The given tree.
+    """
     for word in line_in_file.split():
         node = Node(line_in_file, path)
         if word not in dict.keys():
@@ -45,8 +55,13 @@ def add_new_word_to_data_base(line_in_file, path, dict: {}, trie):
             dict[word].append(node)
 
 
-
 def get_dict_by_offline(dict, dir_name, trie):
+    """
+    This function is the main offline function.
+    :param dict: The given dictionary.
+    :param dir_name: path for our directory that we will map it.
+    :param trie: The given tree.
+    """
     for file_name, stat in mapping_files_in_folders(dir_name):
         with open(file_name, encoding='utf-8') as file:
             for line in file:
